1810413b557ae8cc163ab513084633771490cbe6
SofiaLoven <sofia.loven93@gmail.com>
2023-03-13 09:53:07 +0100

    13/3 morning

--- main.js
+++ main.js
@@ -1,15 +1,12 @@
-let getData = async (url) =>{
-    let data = await fetch(url);
-    let json = await data.json();
-    return json;
-}
 
+
+//Class för karaktärerna.
 class character{
     constructor(name, gender, height, mass, hairColor, skinColor, eyeColor, movies, pictureUrl){
         this.name = name; 
         this.gender = gender;
-        this.height = height;
-        this.mass = mass;
+        this.height = Number(height);
+        this.mass = Number(mass);
         this.hairColor = hairColor;
         this.skinColor = skinColor;
         this.eyeColor = eyeColor;
@@ -19,27 +16,92 @@ class character{
 }
 //New character Bör innegålla promises för att hämta API och sen skickas in i arr. 
 
-let showCharacters = document.querySelector("#characterForm"); //Form! Använd Submit.
-let profileCardOne = document.querySelector("#firstCharacter");
-let profileCardTwo = document.querySelector("#secondCharacter");
+let showCharacters = document.querySelector("#characterForm"); //Referar till ett form.
+let profileCards = document.querySelector("#profileCards");
+let myCharacters = [];
 
-let getCharacthers = async()=>{
-    let params = new URLSearchParams({});
-    // Söker på olika karaktärer och kan hämta dem utifrån Value!! OMG GENI!
-    params.append("search", "chewb");
-    let characters = await getData(`https://swapi.dev/api/people/?${params}`);
-    console.log(characters);
-    console.log("https://swapi.dev/api/people/?" + params);
-    
+//Hämtar de valda värdena från select. Kollar om det är samma värde. Pushar annars in värdet i en Array.
+let choosenCharacters = ()=>{
+    let character1 = document.querySelector('select[name="characterList1"]').value;
+    let character2 = document.querySelector('select[name="characterList2"]').value;
+    if(character1 === character2){
+        alert("Choose two different characters");
+        //Ändra denna till en rolig pop-upp!?
+    } else{
+        myCharacters.push(character1, character2);
+    }
+    allCharacters(myCharacters);
 }
-getCharacthers();
+
+//Kör funktionerna då formuläret skickas. 
+showCharacters.addEventListener("submit",(e)=>{
+    e.preventDefault();
+    choosenCharacters();
+})
+
+//Funktion för att hämta all typ av data.
+let getData = async (url) =>{
+    let data = await fetch(url);
+    let json = await data.json();
+    return json;
+}
+
+//Hämtar datan för valda karaktärer. Använder search för att finna karaktärerna.
+let getCharacther = async(id)=>{
+    try{
+        let response = await getData(`https://swapi.dev/api/people?search=${id}`);
+        return response;
+    }catch(err){
+        console.log("error", err);
+    }
+};
+
+let allCharacters = async (arr) =>{
+    try{
+        let promises = arr.map((id)=> getCharacther(id));
+        let results = await Promise.all(promises);
+        console.log(results);
+        renderCharacters(results);
+    } catch(err){
+        console.log("error", err);
+    }
+};
+
+
+
+let renderCharacters = (characters) => {
+    characters.forEach((obj) => {
+        console.log(obj.results[0].name); //Objektet ligger i en array. Men det finns bara en. Därav index 0.
+        //Använd class nu för att få värden.
+        let p = document.createElement("p");
+        p.innerText= `Name: ${obj.results[0].name}`;
+        profileCards.append(p);
+    });
+}; 
+
 //Hämta karaktärerna med api. Använd promises för att välja karaktär efter listorna?
 
-//Lägg karaktärerna i en array och loopa igenom för att skriva ut i DOMen. 
+//Lägg karaktärerna i en array och loopa igenom för att skriva ut i DOMen. Skapa ny div per profil och skriv ut i
 
 /*
  https://swapi.dev/api
- Skapa en klass som du döper till Character med egenskaperna för 
- name, gender, height, mass,
- hairColor, height, skinColor, eyeColor, movies samt pictureUrl.
+
+let getCharacthers = async(id)=>{
+    let params = new URLSearchParams({});
+    // Söker på olika karaktärer och kan hämta dem utifrån Value!! OMG GENI!
+    params.append("search", id);
+    let character = await getData(`https://swapi.dev/api/people/?${params}`);
+    console.log(character);
+    console.log("https://swapi.dev/api/people/?" + params);
+    
+}
+
+//getCharacthers("yoda"); 
+
+//Funktion för att hämta all typ av data.
+let getData = async (url) =>{
+    let data = await fetch(url);
+    let json = await data.json();
+    return json;
+}
 */
